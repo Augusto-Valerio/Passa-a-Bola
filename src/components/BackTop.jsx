e104eded1332a0f4f3b0c95db0479c8e1cd77159
Augusto Valerio <augusto.valerio06@gmail.com>
2025-09-16 07:52:57 -0300

    fix: z-index of backTop component

--- src/components/BackTop.jsx
+++ src/components/BackTop.jsx
@@ -6,7 +6,7 @@ const BackTop = () => (
   <FloatButton.BackTop
     icon={<ArrowUpOutlined />}
     duration={0}
-    className=" hover:!bg-pink !shadow-[0px_5px_15px_rgba(0,0,0,0.35)]  z-0"
+    className=" hover:!bg-pink !shadow-[0px_5px_15px_rgba(0,0,0,0.35)] !z-10"
   />
 );
 
